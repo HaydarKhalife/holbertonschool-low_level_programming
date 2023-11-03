026cbec891b5c58b121aa80c81037dde4c7c088f
Haydar <Haydarkhalife59@gmail.com>
2023-11-03 11:42:59 -0700

    hi

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,42 +1,42 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 (Success) or 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-    int cents, coins;
+	int cents, coins = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    cents = atoi(argv[1]);
-    coins = 0;
+	cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (cents < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		coins += cents / 25;
+		cents %= 25;
+		coins += cents / 10;
+		cents %= 10;
+		coins += cents / 5;
+		cents %= 5;
+		coins += cents / 2;
+		cents %= 2;
+		coins += cents;
+		printf("%d\n", coins);
+	}
 
-    while (cents > 0)
-    {
-        if (cents >= 25)
-            cents -= 25;
-        else if (cents >= 10)
-            cents -= 10;
-        else if (cents >= 5)
-            cents -= 5;
-        else if (cents >= 2)
-            cents -= 2;
-        else
-            cents -= 1;
-        coins++;
-    }
-
-    printf("%d\n", coins);
-
-    return (0);
+	return (0);
 }
